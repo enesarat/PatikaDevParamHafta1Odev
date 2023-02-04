2ae4fe2892ce21c88dcc2ed58b9c81cf135b9312
Enes Arat <enes_arat@outlook.com>
2023-02-04 15:43:43 +0300

    Initial commit. Contains: Product model has created.

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace PatikaDevParamHafta1Odev.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+
+    }
+}

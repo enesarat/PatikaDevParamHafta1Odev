ac1939ed7c9e59de69fc1234c61dbb59eccc9111
Enes Arat <enes_arat@outlook.com>
2023-02-05 16:26:18 +0300

    Contains: For the product model, mandatory fields were checked according to the properties.

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,11 +1,26 @@
-﻿namespace PatikaDevParamHafta1Odev.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PatikaDevParamHafta1Odev.Models
 {
     public class Product
     {
+        [Required]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 60, MinimumLength = 10, ErrorMessage = "Invalid name lenght!")]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 100, MinimumLength = 100, ErrorMessage = "Invalid description lenght!")]
         public string Description { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 30, MinimumLength = 5, ErrorMessage = "Invalid category name lenght!")]
         public string CategoryName { get; set; } = string.Empty;
+
+        [Required]
+        [Range(minimum: 5, maximum: 500, ErrorMessage = "Invalid quantity! Product quantitiy must be between 5 to 500.")]
         public int Quantity { get; set; }
 
     }
